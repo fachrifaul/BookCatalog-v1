272ba1ea2f1c898eac48eceb5e4579ec50956664
Fachri Febrian <fachripaul@gmail.com>
2017-01-19 20:32:11 +0700

    request data json

--- app/src/main/java/xyz/fatahillah/bookcatalog/MainActivity.java
+++ app/src/main/java/xyz/fatahillah/bookcatalog/MainActivity.java
@@ -4,14 +4,26 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -61,25 +73,163 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected ArrayList<Book> doInBackground(URL... urls) {
+
+            searchKeyword = searchKeyword.replace(" ", "+");
+            URL url = createUrl(BOOK_REQUEST_URL + searchKeyword);
+
+            String jsonResponse = "";
+            try {
+                jsonResponse = makeHttpRequest(url);
+                // the function will be declared later. `makeHttpRequest()` return String formated JSON.
+            } catch (IOException e) {
+                Log.e("MainActivity", "IOException", e);
+                // if error happened, we log the error
+            }
+
+            ArrayList<Book> books =
+                    extractBookInfoFromJson(jsonResponse); // method declared later
+
+            return books;
         }
 
         @Override
         protected void onPostExecute(ArrayList<Book> bookList) {
             progressDialog.dismiss();
 
+            if (bookList == null) {
+                bookAdapter = new BookAdapter(MainActivity.this, new ArrayList<Book>());
+                listView.setAdapter(bookAdapter);
+                return;
+            }
+
+            bookAdapter = new BookAdapter(MainActivity.this, bookList);
+            listView.setAdapter(bookAdapter);
         }
 
     }
 
     private URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException exception) {
+            Log.e("MainActivity", "Error with creating URL", exception);
+            return null;
+        }
+
+        return url;
     }
 
     private String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+                // read the response data and make it as a single string
+            } else {
+                Log.e("MainActivity", "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e("MainActivity", "Problem retrieving the book JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // function must handle java.io.IOException here
+                inputStream.close();
+            }
+        }
+
+        return jsonResponse;
     }
 
+    //
     private String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream,
+                    Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+
+        return output.toString();
     }
 
-    private ArrayList<Book> extractBookInfoFromJson(String bookJSON) {}
+    //
+    private ArrayList<Book> extractBookInfoFromJson(String bookJSON) {
+        if (TextUtils.isEmpty(bookJSON)) {
+            return null;
+        }
+
+        ArrayList<Book> books = new ArrayList<Book>();
+
+        try {
+
+            JSONObject baseJsonResponse = new JSONObject(bookJSON);
+            if (baseJsonResponse.getInt("totalItems") == 0) {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Result not found.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                return null;
+            }
+
+            JSONArray itemArray = baseJsonResponse.getJSONArray("items");
+            for (int i = 0; i < itemArray.length(); i++) {
+                JSONObject cuurentItem = itemArray.getJSONObject(i);
+                JSONObject bookInfo = cuurentItem.getJSONObject("volumeInfo");
+
+                // Get the book title
+                String title = bookInfo.getString("title");
+
+                // Get the list of book's author(s)
+                String[] authors = new String[]{};
+                JSONArray authorJsonArray = bookInfo.optJSONArray("authors");
+                if (authorJsonArray != null) {
+                    ArrayList<String> authorList = new ArrayList<String>();
+                    for (int j = 0; j < authorJsonArray.length(); j++) {
+                        authorList.add(authorJsonArray.get(j).toString());
+                    }
+                    authors = authorList.toArray(new String[authorList.size()]);
+                }
+
+                // Get the book's description
+                String description = "";
+                if (bookInfo.optString("description") != null)
+                    description = bookInfo.optString("description");
+
+                String infoLink = "";
+                if (bookInfo.optString("infoLink") != null)
+                    infoLink = bookInfo.optString("infoLink");
+
+                // Add book information info into books array
+                books.add(new Book(title, authors, description, infoLink));
+            }
+
+        } catch (JSONException e) {
+            Log.e("MainActivity", "Problem parsing the book JSON results", e);
+        }
+
+        return books;
+
+    }
 
 }
